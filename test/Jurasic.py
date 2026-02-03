4441e2b3a966918d75689e38e825179b50881fad
sc <xldsx@duck.com>
2026-02-03 13:53:16 +0100

    Merge branch 'main' into Javi
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	.idea/workspace.xml
    #	selenium-screenshot-1.png
    #	selenium-screenshot-2.png
    #	selenium-screenshot-3.png
    #	selenium-screenshot-4.png

---
+++ test/Jurasic.py
@@ -0,0 +1,46 @@
+import asyncio
+import re
+from playwright.async_api import Playwright, async_playwright, expect
+
+
+async def run(playwright: Playwright) -> None:
+    browser = await playwright.chromium.launch(headless=False)
+    context = await browser.new_context()
+    page = await context.new_page()
+    await page.goto("http://127.0.0.1:8000/jurap.html")
+    await page.get_by_role("link", name="Login").click()
+    await page.get_by_role("link", name="Register").click()
+    await page.get_by_role("textbox", name="Username:").click()
+    await page.get_by_role("textbox", name="Username:").fill("culoconcaca")
+    await page.get_by_role("textbox", name="Username:").press("Tab")
+    await page.get_by_role("textbox", name="Password:").fill("cacaconculo")
+    await page.get_by_role("button", name="Register").click()
+
+    await expect(page.get_by_role("button", name="Register")).to_be_hidden()
+
+    await page.get_by_role("textbox", name="Username:").click()
+    await page.get_by_role("textbox", name="Username:").fill("culoconcaca")
+    await page.get_by_role("textbox", name="Password:").click()
+    await page.get_by_role("textbox", name="Password:").fill("cacaconculo")
+    await page.locator('//*[@id="login-form"]/button').click()
+    await expect(page.get_by_text("Welcome to Jurasstina-Kalle Park!")).to_be_visible()
+    await page.get_by_role("link", name="Buy Tickets", exact=True).click()
+    await page.get_by_role("spinbutton", name="Quantity:").click()
+    await page.get_by_role("spinbutton", name="Quantity:").fill("99999999999999999999999999999999999999999999999")
+    page.once("dialog", lambda dialog: dialog.dismiss())
+    await page.get_by_role("button", name="Add to Cart").click()
+    await page.get_by_role("link", name="Cart").click()
+    page.once("dialog", lambda dialog: dialog.dismiss())
+    await page.get_by_role("button", name="Proceed to Checkout").click()
+
+    # ---------------------
+    await context.close()
+    await browser.close()
+
+
+async def main() -> None:
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+
+asyncio.run(main())
