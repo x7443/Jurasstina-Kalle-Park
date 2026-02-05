babb196ba5ae7495581842120114b6d5a84008a4
Javier <Javier.Bernabe@iths.se>
2026-02-05 09:47:04 +0100

    abrir reserva safari

--- test/Jurasic.py
+++ test/Jurasic.py
@@ -1,21 +1,18 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def test_example(page: Page) -> None:
-    page.goto("file:///C:/WebTesting/Jurasstina-Kalle-Park/site/jurap.html")
-    page.get_by_role("link", name="Home").click()
-    page.get_by_role("link", name="Register").click()
-    page.get_by_role("link", name="Home").click()
-    page.get_by_role("link", name="Register").click()
-    page.get_by_role("textbox", name="Username:").click()
-    page.get_by_role("textbox", name="Username:").fill("Paco Javi")
-    page.get_by_role("textbox", name="Password:").click()
-    page.get_by_role("textbox", name="Password:").fill("11111111")
-    page.get_by_role("button", name="Register").click()
-    page.get_by_role("link", name="Login").click()
-    page.get_by_role("textbox", name="Username:").click()
-    page.get_by_role("textbox", name="Username:").fill("Paco Javi")
-    page.get_by_role("textbox", name="Password:").click()
-    page.get_by_role("textbox", name="Password:").fill("11111111")
-    page.get_by_role("button", name="Login").click()
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://127.0.0.1:8000/jurap.html")
+    page.get_by_role("link", name="Book Safaris").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
