0536152b54617aea8a6ae0084db924795fe9487f
Javier <Javier.Bernabe@iths.se>
2026-02-06 11:59:30 +0100

    test home commingsoon

--- test/test_register_password_too_short.py
+++ test/test_register_password_too_short.py
@@ -6,16 +6,26 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+
+    # Open web sidan frän lokal på datum.
     page.goto("http://127.0.0.1:8000/jurap.html")
+
+    # Klickar på "Register" för att öppna registreringsformuläret.
     page.get_by_role("link", name="Register").click()
+
+    # Skriva en användarnamn och en password med färre än 8 tecken och clicka Register.
     page.get_by_role("textbox", name="Username:").click()
     page.get_by_role("textbox", name="Username:").fill("Javilon9")
     page.get_by_role("textbox", name="Password:").click()
     page.get_by_role("textbox", name="Password:").fill("Panta")
+
+    # Klickar på "Register".
     page.get_by_role("button", name="Register").click()
+
+    # Verifierar att ett varningsmeddelande visas om lösenordet är kortare än 8 tecken
     expect(page.get_by_text("Password must be at least 8 characters long.")).to_be_visible()
 
-    # ---------------------
+
     context.close()
     browser.close()
 
