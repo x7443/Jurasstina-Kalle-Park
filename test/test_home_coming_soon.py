7e132113b091354394906ec898ab31624428c350
Javier <Javier.Bernabe@iths.se>
2026-02-10 09:37:29 +0100

    test home commingsoon

--- test/test_home_coming_soon.py
+++ test/test_home_coming_soon.py
@@ -6,7 +6,7 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("http://127.0.0.1:8000/jurap.html")
+    page.goto("http://127.0.0.1:8000/jurap.html" )
     page.get_by_role("link", name="Coming Soon!").click()
 
     # ---------------------
