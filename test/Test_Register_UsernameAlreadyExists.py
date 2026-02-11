52c51353b81e546f6f8d59d66504d25556cad7bb
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2026-02-11 12:59:24 +0100

    Text med förklaring av vilka steg tester gör samt @tag tillagd

--- test/Test_Register_UsernameAlreadyExists.py
+++ test/Test_Register_UsernameAlreadyExists.py
@@ -8,14 +8,14 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("http://127.0.0.1:8000/jurap.html")
-    page.get_by_role("link", name="Register").click()
-    page.get_by_role("textbox", name="Username:").click()
-    page.get_by_role("textbox", name="Username:").fill("User1")
-    page.get_by_role("textbox", name="Username:").press("Tab")
-    page.get_by_role("textbox", name="Password:").fill("1234ABCD")
-    page.get_by_role("button", name="Register").click()
-    page.get_by_role("link", name="Register").click()
+    page.goto("http://127.0.0.1:8000/jurap.html") #Går till hemsidan
+    page.get_by_role("link", name="Register").click() #man klickar på register
+    page.get_by_role("textbox", name="Username:").click() #Man klickar på fältet användarnamn
+    page.get_by_role("textbox", name="Username:").fill("User1") #man matar in fältet username med tecknen "User1"
+    page.get_by_role("textbox", name="Username:").press("Tab")#Man tabbar till nästa fält: password
+    page.get_by_role("textbox", name="Password:").fill("1234ABCD")#Man skriver in ett lösenord
+    page.get_by_role("button", name="Register").click()#Man klickar på knappen "Register" för att registrera sig och vidarebefordras till "Login"
+    page.get_by_role("link", name="Register").click()# Resten av koden är upprepning av stegen innan
     page.get_by_role("textbox", name="Username:").click()
     page.get_by_role("textbox", name="Username:").fill("User1")
     page.get_by_role("textbox", name="Username:").press("Tab")
