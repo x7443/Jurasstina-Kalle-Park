7c5907fb114cbc4f79e1c5d8e415686b9bd822ee
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2026-02-12 14:58:23 +0100

    Två tester färdiga; 1. När man är inloggad och misslyckas boka safari. 2. När man är inloggad, bokar biljetter och lyckas boka safari. Villkoret för att lyckas är att man behöver ha lagt till biljetter. För någon anledning går testet igenom om jag jag har kodsnutten slow_mo= 2000. Ifall jag inte har det så misslyckas testet, eller ifall värdet i slow_mo är för lågt.

---
+++ test/Test_BookSafariWhileLoggedIn_FailedMessage.py
@@ -0,0 +1,50 @@
+import re
+from csv import excel
+import pytest
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+@pytest.mark.VG_Almedin
+def run(playwright: Playwright) -> None:
+
+    #Jag fattar inte varför tester går igenom med slow mo = 2000 och inte när man kör det utan slow_mo=
+    browser = playwright.chromium.launch(headless=False, slow_mo= 2000)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://127.0.0.1:8000/jurap.html")
+
+    # Kod för registrering och login, förberedelse för test; START
+    page.get_by_role("link", name="Register").click()
+    page.get_by_role("textbox", name="Username:").click()
+    page.get_by_role("textbox", name="Username:").fill("User1")
+    page.get_by_role("textbox", name="Username:").press("Tab")
+    page.get_by_role("textbox", name="Password:").fill("12345678")
+    page.get_by_role("button", name="Register").click()
+
+    expect(page.get_by_role("Heading", name="Login")).to_be_visible()
+
+    page.get_by_role("textbox", name="Username:").click()
+    page.get_by_role("textbox", name="Username:").fill("User1")
+    page.get_by_role("textbox", name="Username:").press("Tab")
+    page.get_by_role("textbox", name="Password:").fill("12345678")
+    page.get_by_role("button", name="Login").click() # Förberedelse SLUT
+
+    # TEST BÖRJA HÄR
+    page.get_by_role("link", name="Book Safaris").click() # Användare trycker på flik "Book Safari"
+    page.get_by_role("textbox", name="Select Safari Date:").fill("2026-02-18") # Användare trycker på kalenderikonen och väljer datum i fältet "Select Safari date"
+    page.get_by_role("button", name="Add to Cart").click() # Användare trycker på knapp "Add to cart" för att lägga till vad safaribokning
+
+    # Resultat: Följande felmeddelande med en ruta dyker upp:
+    expect(page.get_by_text("You must purchase a general admission ticket before booking a safari.")).to_be_visible()
+
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
+
+
---
+++ test/Test_BookSafariWhileLoggedIn_Succes.py
@@ -0,0 +1,59 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+
+    browser = playwright.chromium.launch(headless=False, slow_mo=2000)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://127.0.0.1:8000/jurap.html")
+
+    #Kod för registrering och login, förberedelse för test; START
+    page.get_by_role("link", name="Register").click()
+    page.get_by_role("textbox", name="Username:").click()
+    page.get_by_role("textbox", name="Username:").fill("User1")
+    page.get_by_role("textbox", name="Username:").press("Tab")
+    page.get_by_role("textbox", name="Password:").fill("12345678")
+    page.get_by_role("button", name="Register").click()
+
+    expect(page.get_by_role("Heading", name="Login")).to_be_visible()
+
+    page.get_by_role("textbox", name="Username:").click()
+    page.get_by_role("textbox", name="Username:").fill("User1")
+    page.get_by_role("textbox", name="Username:").press("Tab")
+    page.get_by_role("textbox", name="Password:").fill("12345678")
+    page.get_by_role("button", name="Login").click() #Förberedelse SLUT
+
+    # TEST BÖRJAR HÄR - Användare trycker på flik "Buy tickets"
+    page.get_by_role("link", name="Buy Tickets", exact=True).click()
+
+    page.once("dialog", lambda dialog: dialog.dismiss()) # Biljettyp: vuxen, Ailjettkategori: vanlig, Antal biljetter: 1
+    page.get_by_role("button", name="Add to Cart").click() # Användare trycker på knappen "Add to cart" för att lägga till vald biljett i kundvagnen
+    page.get_by_role("link", name="Book Safaris").click() # Användare trycker på flik "Book safari" för att boka safarityp
+    page.get_by_role("textbox", name="Select Safari Date:").fill("2026-02-18")  #Användare trycker på kalenderikonen och väljer ett datum framåt i tiden i kalender i fältet "Select safari Date"
+    page.once("dialog", lambda dialog: dialog.dismiss()) # Användaren väljer den förvalda safaritypen "Herbivore tour" i fältet "Select Safari Type"
+    page.get_by_role("button", name="Add to Cart").click() # Användaren trycker på knappen "Add to Cart" för att lägga till bokning av valt datum och vald safarityp
+
+    # Användaren trycker på knappen "Cart"
+    page.get_by_role("link", name="Cart").click()
+    page.once("dialog", lambda dialog: dialog.dismiss())
+
+    # Resultat: Man ser en överblick med information om tillagda biljetter och bokade safarityper
+
+    # Användaren trycker på knappen "Proceed to checkout" för att fullfölja köpet av innehållet i kundvagnen
+    page.get_by_role("button", name="Proceed to Checkout").click()
+
+    # Resultat: Köpet lyckas och går igenom!
+
+    # Användaren trycker på kundvagnen "Cart" igen för att säkerställa att kundvagnen är tom, slut på test
+    page.get_by_role("link", name="Cart").click()
+    page.once("dialog", lambda dialog: dialog.dismiss())
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
