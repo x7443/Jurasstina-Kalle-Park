eb82d39e509fd1ee3b970315ffd9dacc16d552d5
sc <xldsx@duck.com>
2026-02-16 11:38:07 +0100

    fix

--- test/test_kopa_regular_biljet_vadagar.py
+++ test/test_kopa_regular_biljet_vadagar.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 #Test-ID: TC-SAF-02.2-POS
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
 
@@ -20,23 +20,27 @@ def run(playwright: Playwright) -> None:
     page.get_by_role("button", name="Register").click()
 
     # Skriva en användarnamn och en password för att "loga in".
+    page.get_by_role("link", name="Login").click()
     page.get_by_role("textbox", name="Username:").click()
     page.get_by_role("textbox", name="Username:").fill("Javi10")
     page.get_by_role("textbox", name="Password:").click()
     page.get_by_role("textbox", name="Password:").fill("965312396")
     page.get_by_role("button", name="Login").click()
+    expect(page.get_by_text("Welcome to Jurasstina-Kalle Park!")).to_be_visible()
 
     # Väljer en Regular-biljett för att köpa den och lägga till den i varukorgen.
     page.get_by_role("link", name="Buy Tickets", exact=True).click()
-    page.once("dialog", lambda dialog: dialog.dismiss())
     page.get_by_role("button", name="Add to Cart").click()
 
     # Väljer en Safari och ett datum under veckan för att köpa den och lägga till den i varukorgen.
     page.get_by_role("link", name="Book Safaris").click()
     page.get_by_role("textbox", name="Select Safari Date:").fill("2026-12-14")
     page.get_by_label("Select Safari Type:").select_option("T-Rex Rumble")
-    page.once("dialog", lambda dialog: dialog.dismiss())
     page.get_by_role("button", name="Add to Cart").click()
+
+    page.get_by_role("link", name="Cart").click()
+    expect(page.get_by_text("T-Rex Rumble on 2026-12-14")).to_be_visible()
+
 
     # ---------------------
     context.close()
