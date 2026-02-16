3c6eebebf26cd283ae9ead33b051159e3cb6dc66
sc <xldsx@duck.com>
2026-02-16 11:23:14 +0100

    fix

--- test/test_book_safari_fell_biljet.py
+++ test/test_book_safari_fell_biljet.py
@@ -6,7 +6,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 @pytest.mark.VG_Javi
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False,slow_mo=5000)
+    browser = playwright.chromium.launch(headless=False,slow_mo=700)
     context = browser.new_context()
     page = context.new_page()
 
@@ -16,23 +16,29 @@ def run(playwright: Playwright) -> None:
     # Klickar på "Register" för att öppna registreringsformuläret.
     page.get_by_role("link", name="Register").click()
 
-    # Skriva en användarnamn och en password för att "registrera sig".
+    # Skriva en användarnamn och en password och clicka Register.
+    page.get_by_role("link", name="Register").click()
     page.get_by_role("textbox", name="Username:").click()
-    page.get_by_role("textbox", name="Username:").fill("PacoJavi10")
+    page.get_by_role("textbox", name="Username:").fill("Javi10")
     page.get_by_role("textbox", name="Password:").click()
     page.get_by_role("textbox", name="Password:").fill("965312396")
     page.get_by_role("button", name="Register").click()
+    expect(page.get_by_role("heading", name="Login")).to_be_visible()
 
-    # Skriva en användarnamn och en password för att  "logga in".
+
+    # Skriva en användarnamn och en password för att "loga in".
+    page.get_by_role("link", name="Login").click()
     page.get_by_role("textbox", name="Username:").click()
-    page.get_by_role("textbox", name="Username:").fill("PacoJavi10")
+    page.get_by_role("textbox", name="Username:").fill("Javi10")
     page.get_by_role("textbox", name="Password:").click()
     page.get_by_role("textbox", name="Password:").fill("965312396")
     page.get_by_role("button", name="Login").click()
+    expect(page.get_by_text("Welcome to Jurasstina-Kalle Park!")).to_be_visible()
+
+    ### HÄR BÖRJAR TESTET ###
 
     #Väljer en Regular-biljett för att köpa den och lägga till den i varukorgen.
     page.get_by_role("link", name="Buy Tickets", exact=True).click()
-    page.once("dialog", lambda dialog: dialog.dismiss())
     page.get_by_role("button", name="Add to Cart").click()
     page.get_by_role("link", name="Book Safaris").click()
     page.get_by_role("textbox", name="Select Safari Date:").fill("2026-12-19")
@@ -41,7 +47,7 @@ def run(playwright: Playwright) -> None:
 
 
     #Felvalidering: systemet visar ett felmeddelande
-    expect(page.get_by_text("VIP tickets required to book safaris on weekends.")).to_be_visible(timeout=5000)
+    expect(page.get_by_text("VIP tickets required to book safaris on weekends.")).to_be_visible
 
 
     # ---------------------
